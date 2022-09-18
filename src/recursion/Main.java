82d36e419c09f0c53985c7e2fe507eb14dbd4a99
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-09-19 00:02:15 +0900

    입력 스트림 수정
    This reverts commit 22527004f2821950c8f3d293763ebf21bc94d6cd.

--- src/recursion/Main.java
+++ src/recursion/Main.java
@@ -1,33 +1,34 @@
 package recursion;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Map map = new Map(8, 8);
 		Mouse mouse = new Mouse();
-		
+
 		mouse.move(setCheeseLocation(map.getX(), map.getY()), map);
 	}
 
-	private static Cheese setCheeseLocation(int mapX, int mapY) {
-		Scanner input = new Scanner(System.in);
+	private static Cheese setCheeseLocation(int mapX, int mapY) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Cheese cheese = new Cheese();
 
 		int cheeseX;
 		int cheeseY;
 		boolean flag = true;
-		
+
 		while (flag) {
 			System.out.println("Cheese의 위치 좌표를 입력 해주세요.");
 			System.out.print("x : ");
-			cheeseX = input.nextInt();
+			cheeseX = Integer.parseInt(br.readLine());
 			System.out.print("y : ");
-			cheeseY = input.nextInt();
-			
-			if((cheeseX > mapX || cheeseY > mapY)
-					|| (cheeseX < 0 || cheeseY < 0)) {
+			cheeseY = Integer.parseInt(br.readLine());
+
+			if ((cheeseX > mapX || cheeseY > mapY) || (cheeseX < 0 || cheeseY < 0)) {
 				System.out.println("Cheese의 위치가 맵의 범위를 벗어났습니다.");
 			} else {
 				cheese.setLocationX(cheeseX);
@@ -35,8 +36,7 @@ public class Main {
 				flag = false;
 			}
 		}
-		
-		input.close();
+
 		return cheese;
 	}
 
